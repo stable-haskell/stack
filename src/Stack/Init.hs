5fc825bdb987bf62a1adf9863ae8160782d9e095
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2024-01-30 23:34:38 +0000

    Re #6466 Update stack init

--- src/Stack/Init.hs
+++ src/Stack/Init.hs
@@ -405,7 +405,7 @@ renderStackYaml p ignoredPackages dupPackages =
   -- Per Section Help
   comments =
     [ ("user-message"     , userMsgHelp)
-    , ("resolver"         , resolverHelp)
+    , ("snapshot"         , snapshotHelp)
     , ("packages"         , packageHelp)
     , ("extra-deps"       , extraDepsHelp)
     , ("flags"            , "# Override default flag values for local packages and extra-deps")
@@ -419,20 +419,20 @@ renderStackYaml p ignoredPackages dupPackages =
     , "For advanced use and comprehensive documentation of the format, please see:"
     , "https://docs.haskellstack.org/en/stable/yaml_configuration/"
     ]
-  resolverHelp = commentHelp
-    [ "Resolver to choose a 'specific' stackage snapshot or a compiler version."
+  snapshotHelp = commentHelp
+    [ "A 'specific' Stackage snapshot or a compiler version."
     , "A snapshot resolver dictates the compiler version and the set of packages"
     , "to be used for project dependencies. For example:"
     , ""
-    , "resolver: lts-22.7"
-    , "resolver: nightly-2024-01-20"
-    , "resolver: ghc-9.6.4"
+    , "snapshot: lts-22.7"
+    , "snapshot: nightly-2024-01-20"
+    , "snapshot: ghc-9.6.4"
     , ""
     , "The location of a snapshot can be provided as a file or url. Stack assumes"
     , "a snapshot provided as a file might change, whereas a url resource does not."
     , ""
-    , "resolver: ./custom-snapshot.yaml"
-    , "resolver: https://example.com/snapshots/2023-01-01.yaml"
+    , "snapshot: ./custom-snapshot.yaml"
+    , "snapshot: https://example.com/snapshots/2023-01-01.yaml"
     ]
   userMsgHelp = commentHelp
     [ "A warning or info to be displayed to the user on config load." ]
